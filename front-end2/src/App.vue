0d03066a8570c2c3c572d38a7f789af4a44896c5
AlexLam1997 <alexander.lam@mail.mcgill.ca>
2019-10-17 14:21:18 -0400

    reinit vue front end

---
+++ front-end2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ImageUpload/>
+    <ImageDisplay/>
+  </div>
+</template>
+
+<script>
+import ImageUpload from './components/ImageUpload.vue'
+import ImageDisplay from './components/ImageDisplay.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ImageUpload, 
+    ImageDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
